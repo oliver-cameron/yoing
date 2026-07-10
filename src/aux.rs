18d35bb83f48f869ffe0529622485c5b9910e9b2
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2026-07-10 22:15:11 +1000

    fleshed out thing, added some matrix multiplications

--- src/aux.rs
+++ src/aux.rs
@@ -13,13 +13,20 @@ pub struct SafeIndex<const Max: usize>(pub usize);
 // }
 pub type Lin = FixedI8<U4>;
 #[derive(Clone, Copy)]
-pub struct Point {x: f64, y: f64}
+pub struct Point {pub x: f64, pub y: f64}
 #[derive(Clone, Copy)]
 pub enum Weight {Zero, Some {parr: Lin, perp: Lin}}
-pub struct State<const LENGTH: usize> {points: [Point; LENGTH]}
+pub struct State<const LENGTH: usize> {pub points: [Point; LENGTH]}
+
+#[derive(Clone, Copy)]
+pub struct IOPoint {xx: f64, xy: f64, yx: f64, yy: f64}
+#[derive(Clone, Copy)]
+pub struct IOState<const LENGTH: usize> {pub matrix: [[IOPoint; LENGTH]; LENGTH]}
+pub struct Matrix<const DIMENSION: usize> {pub contents: [[f64; DIMENSION]; DIMENSION]}
+
 #[derive(Copy, Clone)]
-pub struct AuxRow<const INLENGTH: usize> {weights: [Weight; INLENGTH]}
-pub struct AuxState<const INLENGTH: usize, const OUTLENGTH: usize> {transform: [AuxRow<INLENGTH>; OUTLENGTH]}
+pub struct AuxRow<const INLENGTH: usize> {pub weights: [Weight; INLENGTH]}
+pub struct AuxState<const INLENGTH: usize, const OUTLENGTH: usize> {pub transform: [AuxRow<INLENGTH>; OUTLENGTH]}
 
 impl <const INLENGTH: usize> AuxRow<INLENGTH>{
     pub fn new(contents: [Weight; INLENGTH]){
@@ -52,5 +59,94 @@ impl <const LENGTH: usize> State<LENGTH>{
                     }
                 }))}
     }
+    pub fn backward_aux<const INLENGTH: usize>(&self, aux_state: &AuxState<INLENGTH, LENGTH>) -> State<INLENGTH> {
+        let mut out_points = [Point { x: 0.0, y: 0.0 }; INLENGTH];
+        for (out_idx, out_row) in aux_state.transform.iter().enumerate() {
+            let current_p = self.points[out_idx];
+            for (in_idx, in_weight) in out_row.weights.iter().enumerate() {
+                match in_weight {
+                    Weight::Zero => {}
+                    Weight::Some { parr, perp } => {
+                        let parr_f64 = parr.to_num::<f64>();
+                        let perp_f64 = perp.to_num::<f64>();
+                        out_points[in_idx].x += current_p.x * parr_f64 + current_p.y * perp_f64;
+                        out_points[in_idx].y += current_p.y * parr_f64 - current_p.x * perp_f64;
+                    }
+                }
+            }
+        }
+
+        State { points: out_points }
+    }
 }
 
+impl <const LENGTH: usize> IOState<LENGTH> {
+    pub fn multiply_column (&self, input_state: &State<LENGTH>) -> State<LENGTH>{
+        State{ points: self.matrix.map(|out| out.iter().enumerate().fold(
+                Point{x:0.,y:0.,},
+                |acc, (in_index, in_point)| {
+                    Point{x: acc.x + input_state.points[in_index].x * in_point.xx + input_state.points[in_index].y * in_point.yx,
+                          y: acc.y + input_state.points[in_index].x * in_point.xy + input_state.points[in_index].y * in_point.yy}
+                }))}
+    }
+        pub fn backward_aux<const INLENGTH: usize>(&self, aux_state: &AuxState<INLENGTH, LENGTH>) -> IOState<INLENGTH> {
+        // 1. Right multiplication first: Middle = M * A
+        // Size: [LENGTH x LENGTH] * [LENGTH x INLENGTH] -> [LENGTH x INLENGTH]
+        let mut middle_matrix = [[IOPoint { xx: 0.0, xy: 0.0, yx: 0.0, yy: 0.0 }; INLENGTH]; LENGTH];
+        
+        for r in 0..LENGTH {
+            for c in 0..INLENGTH {
+                let mut sum = IOPoint { xx: 0.0, xy: 0.0, yx: 0.0, yy: 0.0 };
+                for k in 0..LENGTH {
+                    let m_point = self.matrix[r][k];
+                    match &aux_state.transform[k].weights[c] {
+                        Weight::Zero => {}
+                        Weight::Some { parr, perp } => {
+                            let parr_f64 = parr.to_num::<f64>();
+                            let perp_f64 = perp.to_num::<f64>();
+                            
+                            // M * A (Standard block multiplication)
+                            sum.xx += m_point.xx * parr_f64 - m_point.xy * perp_f64;
+                            sum.xy += m_point.xx * perp_f64 + m_point.xy * parr_f64;
+                            sum.yx += m_point.yx * parr_f64 - m_point.yy * perp_f64;
+                            sum.yy += m_point.yx * perp_f64 + m_point.yy * parr_f64;
+                        }
+                    }
+                }
+                middle_matrix[r][c] = sum;
+            }
+        }
+
+        // 2. Left multiplication by Transpose: Out = A^T * Middle
+        // Size: [INLENGTH x LENGTH] * [LENGTH x INLENGTH] -> [INLENGTH x INLENGTH]
+        let mut out_matrix = [[IOPoint { xx: 0.0, xy: 0.0, yx: 0.0, yy: 0.0 }; INLENGTH]; INLENGTH];
+        
+        for r in 0..INLENGTH {
+            for c in 0..INLENGTH {
+                let mut sum = IOPoint { xx: 0.0, xy: 0.0, yx: 0.0, yy: 0.0 };
+                for k in 0..LENGTH {
+                    // Notice the outer transpose: reading row 'k', col 'r' from aux matrix
+                    match &aux_state.transform[k].weights[r] {
+                        Weight::Zero => {}
+                        Weight::Some { parr, perp } => {
+                            let parr_f64 = parr.to_num::<f64>();
+                            let perp_f64 = perp.to_num::<f64>();
+                            let mid = middle_matrix[k][c];
+                            
+                            // A^T * Middle (Inner 2x2 block matrix is ALSO transposed here)
+                            // [ parr  perp] * [xx xy]
+                            // [-perp  parr]   [yx yy]
+                            sum.xx += parr_f64 * mid.xx + perp_f64 * mid.yx;
+                            sum.xy += parr_f64 * mid.xy + perp_f64 * mid.yy;
+                            sum.yx += -perp_f64 * mid.xx + parr_f64 * mid.yx;
+                            sum.yy += -perp_f64 * mid.xy + parr_f64 * mid.yy;
+                        }
+                    }
+                }
+                out_matrix[r][c] = sum;
+            }
+        }
+
+        IOState { matrix: out_matrix }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,54 @@
 use std::ops::Index;
+use std::ops::Range;
 use fixed;
 mod aux;
 pub fn Bmain() {
     println!("Hello, world!");
 }
-pub struct Thing<const STATELENGTH: usize, const AUXLENGTH: usize>{
-    state: aux::State<STATELENGTH>,
+
+pub enum colourStyle{
+    Colour([u8;3])
+}
+
+pub struct Thing<const STATELENGTH: usize, const AUXLENGTH: usize, const SHAPECOUNT: usize>{
+    state_pos: aux::State<STATELENGTH>,
+    state_vel: aux::State<STATELENGTH>,
     aux_config: aux::AuxState<STATELENGTH, AUXLENGTH>,
-    aux_state: aux::State<AUXLENGTH>,
+    aux_pos: aux::State<AUXLENGTH>,
+    aux_vel: aux::State<AUXLENGTH>,
+    matrix_aux_restore: aux::IOState<AUXLENGTH>,
+    matrix_aux_dampen: aux::IOState<AUXLENGTH>,
+    matrix_bias_column: aux::State<AUXLENGTH>,
+    shape_config: [(Range<usize>,colourStyle);SHAPECOUNT],
     // forces?
+}
+
+impl <const STATELENGTH: usize, const AUXLENGTH: usize, const SHAPECOUNT: usize> Thing<STATELENGTH, AUXLENGTH, SHAPECOUNT> {
+    pub fn update_aux(mut self){
+       self.aux_pos = self.state_pos.forward_aux(&self.aux_config); 
+       self.aux_vel = self.state_vel.forward_aux(&self.aux_config); 
+    }
+    pub fn pre_force_calc(self){
+        self.update_aux();
+    }
+    pub fn post_force_calc(self){
+        // Compute residuals of restore and dampen blocks. These are influence matrcies, and should
+        // really just have the difference of pos or vel as input. This probably makes no sense so
+        // come talk to me.
+        let restore_residual: aux::State<AUXLENGTH> = self.matrix_aux_restore.multiply_column(&self.aux_pos);
+        let dampen_residual : aux::State<AUXLENGTH> = self.matrix_aux_dampen .multiply_column(&self.aux_vel);
+        let bias_column_state: aux::State<STATELENGTH> = aux::State{points: std::array::from_fn(|i|
+                {
+                    let orig = self.matrix_bias_column.points[i];
+                    aux::Point{x: orig.x - restore_residual.points[i].x - dampen_residual.points[i].x,
+                       y: orig.y - restore_residual.points[i].y - dampen_residual.points[i].x}
+                }
+        )
+            }.backward_aux(&self.aux_config);
+
+        let matrix_state_restore = self.matrix_aux_restore.backward_aux(&self.aux_config);
+        let matrix_state_dampen  = self.matrix_aux_dampen .backward_aux(&self.aux_config);
+
+        
+    }
 }
